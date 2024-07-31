d9ff17416bcf85e0a73baca2d771845c26440da0
Anxing <anxingxiao@gmail.com>
2024-07-31 21:28:30 +0800

    add det seg

--- service/run_det_seg.sh
+++ service/run_det_seg.sh
@@ -5,12 +5,11 @@
 #SBATCH --mem=20000
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=anxingxiao@gmail.com
-#SBATCH --gres=gpu:2
+#SBATCH --gres=gpu:1
 #SBATCH --nodelist=crane5
 nvidia-smi
 source activate learning
 
-python /data/home/anxing/service/llava/llava_server_hf_110b.py &
 python /data/home/anxing/service/owl_vit/server.py &
 python /data/home/anxing/service/sam/server.py &
 
